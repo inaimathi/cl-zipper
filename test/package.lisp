b1a2f98d269bfa20f20efc1b77d03dcbb2201e81
inaimathi <leo.zovic@gmail.com>
2020-05-06 12:44:53 -0400

    Namespace resolution

--- test/package.lisp
+++ test/package.lisp
@@ -1,4 +1,5 @@
 ;;;; test/package.lisp
 
 (defpackage #:cl-zipper-test
-  (:use #:cl #:cl-zipper #:test-utils))
+  (:use #:cl #:cl-zipper #:test-utils)
+  (:shadowing-import-from #:cl-zipper #:delete #:replace))
